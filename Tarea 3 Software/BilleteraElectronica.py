7f96e3f5bce9590ee93fa2464831832d9de48647
user.Arleyn <arleyn.g.2712@hotmail.com>
2015-05-04 09:54:26 -0430

    Commit

--- Tarea 3 Software/BilleteraElectronica.py
+++ Tarea 3 Software/BilleteraElectronica.py
@@ -1,10 +1,25 @@
 '''
 Created on 3/5/2015
-
 @author: Arleyn Goncalves
          Edwin Murillo
 '''
 
+class DatosCreditos(object):
+    def __init__(self):
+        self._lista_recargas = [];
+        
+    def Almacenar(self,cantidad,fecha,identificar):
+        self._listas_recargas.append((cantidad,fecha,identificar));
+        
+
+class DatosDebitos(object):
+    def __init__(self):
+        self._lista_consumos = [];
+    
+    def Almacenar(self,cantidad,fecha,identificar):
+        self._listas_consumos.append((cantidad,fecha,identificar))
+        
+        
 class BilleteraElectronica(object):
     def __init__(self,identificador,nombre,apellido,CI):
         self._identificador = identificador;
@@ -12,11 +27,13 @@ class BilleteraElectronica(object):
         self._apellido = apellido;
         self._CI = CI;
         self._saldo = 0;
-        
+        self._Consumo = DatosDebitos();
+        self._Creditos = DatosCreditos();
+                
     def saldo(self):
         print(self._saldo);
         
-    def recargar(self,cantidad):
+    def recargar(self,cantidad,):
         if cantidad <= 0:
             raise Exception("La cantidad a recargar tiene que se mayor a 0");
         else:
---
+++ Tarea 3 Software/TestBilleteraElectronica.py
@@ -0,0 +1,21 @@
+'''
+Created on 4/5/2015
+
+@author: personal
+'''
+import unittest
+from BilleteraElectronica import DatosCreditos
+from BilleteraElectronica import DatosDebitos
+
+
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
