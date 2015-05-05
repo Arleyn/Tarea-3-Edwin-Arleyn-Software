6c932f0d25753b4d4715da66d743f03c954bca33
user.Arleyn <arleyn.g.2712@hotmail.com>
2015-05-04 20:23:49 -0430

    Prueba (Sexto test)

--- Tarea3/testBilleteElectronico.py
+++ Tarea3/testBilleteElectronico.py
@@ -33,3 +33,10 @@ class Test(unittest.TestCase):
         saldo = Arleyn.recargar(100,'27/12/1992','01');
         saldo1 = Arleyn.consumir(50,'28/12/1992','02')
         self.assertEqual(50,saldo1);
+        
+    def testConsumirCredito2(self):
+        Arleyn = BilleteraElectronica.BilleteraElectronica("001","Arleyn","Goncalves","21467704");
+        saldo = Arleyn.recargar(100,'27/12/1992','01');
+        saldo1 = Arleyn.consumir(50,'28/12/1992','02')
+        saldo2 = Arleyn.consumir(45,'28/12/1992','02')
+        self.assertEqual(5,saldo2);
