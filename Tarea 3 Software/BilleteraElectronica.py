96715719152b4ee54f5fedd9e51c087942e6fa5d
user.Arleyn <arleyn.g.2712@hotmail.com>
2015-05-04 19:31:49 -0430

    Commit (Algunas actualizaciones 2)

--- Tarea 3 Software/BilleteraElectronica.py
+++ Tarea 3 Software/BilleteraElectronica.py
@@ -6,20 +6,19 @@ Created on 3/5/2015
 
 class DatosCreditos(object):
     def __init__(self):
-        self._lista_recargas = [];
+        self._listas_recargas = [];
         
     def Almacenar(self,cantidad,fecha,identificar):
         self._listas_recargas.append((cantidad,fecha,identificar));
-        return self._listas_recargas;
         
 
 class DatosDebitos(object):
     def __init__(self):
-        self._lista_consumos = [];
+        self._listas_consumos = [];
     
     def Almacenar(self,cantidad,fecha,identificar):
         self._listas_consumos.append((cantidad,fecha,identificar))
-        return self._listas_consumos,
+        return self._listas_consumos
         
         
 class BilleteraElectronica(object):
@@ -29,27 +28,29 @@ class BilleteraElectronica(object):
         self._apellido = apellido;
         self._CI = CI;
         self._saldo = 0;
-        Consumos = DatosDebitos();
-        Creditos = DatosCredito();
-                
+        self._Consumos = DatosDebitos();
+        self._Creditos = DatosCreditos();
+               
     def saldo(self):
         print(self._saldo);
         return self._saldo;
-    
-    def recargar(self,cantidad,):
+        
+    def recargar(self,cantidad,fecha,id_estacionamiento):
         if cantidad <= 0:
             raise Exception("La cantidad a recargar tiene que se mayor a 0");
         else:
             self._saldo += cantidad;
+            self._Creditos.Almacenar(cantidad,fecha,id_estacionamiento);
             return self._saldo;
         
-    def consumir(self,cantidad):
+    def consumir(self,cantidad,fecha,id_estacionamiento):
         if self._saldo < cantidad:
             raise Exception("No tiene saldo suficiente");
         elif cantidad <= 0:
             raise Exception("La cantidad a consumir tiene que ser mayor a 0");
         else:
             self._saldo -= cantidad;
+            self._Consumos.Almacenar(cantidad,fecha,id_estacionamiento);
             return self._saldo;
         
 if __name__ == '__main__':
